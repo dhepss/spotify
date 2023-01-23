b42612fc40ac79ab0bf632f709b01ef8f2471b9c
dheepss <94340764+dhepss@users.noreply.github.com>
2023-01-23 16:26:39 +0700

    Add files via upload

---
+++ js/src.js
@@ -0,0 +1,11 @@
+const button = document.querySelector('.hero-button');
+button.addEventListener('click', () => {
+  // code to navigate to the next page or section
+});
+
+const cards = document.querySelectorAll('.card-button');
+cards.forEach(card => {
+  card.addEventListener('click', () => {
+    // code to play the album or navigate to the album page
+  });
+});
